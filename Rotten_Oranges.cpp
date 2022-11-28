3729237e7b2b7c32288923e661cc5bf7781ee6ee
Soumajyoti Sarkar <103195837+soumajyoti02@users.noreply.github.com>
2022-11-28 17:05:39 +0530

    Update Rotten_Oranges.cpp

--- Rotten_Oranges.cpp
+++ Rotten_Oranges.cpp
@@ -26,6 +26,7 @@ Explanation: The grid is-
 Oranges at (0,0) and (0,1) can't rot orange at
 (0,3).;
 
+========================================================================================
 
 Approach : Make a 2D visited array and store the already rotten oranges. I mean at that index, make vis[i][j] = 2; Store the i and j in a queue also;
 Maintain a part in queue which will take care of times;
@@ -38,78 +39,115 @@ Then simply start the BFS while(!queue.empty())
 
 Return max time variable;
 
+-----------------------------------------------
 
+The algorithm steps are as follows:
 
+---> For BFS traversal, we need a queue data structure and a visited array. Create a replica of the given array, i.e., 
+     create another array of the same size and call it a visited array. We can use the same matrix, but we will avoid alteration of the original data. 
+       
+---> The pairs of cell number and initial time, i.e., <<row, column>, time> will be pushed in the queue and marked as visited 
+     (represents rotten) in the visited array. For example, ((2,0), 0) represents cell (2, 0) and initial time 0.  
+       
+---> While BFS traversal, pop out an element from the queue and travel to all its neighbours. In a graph, we store the 
+      list of neighbours in an adjacency list but here we know the neighbours are in 4 directions. 
+       
+---> We go in all 4 directions and check for valid unvisited fresh orange neighbours. To travel 4 directions we will use nested loops,
+     you can find the implementation details in the code. 
+---> BFS function call will make sure that it starts the BFS call from each rotten orange cell, and rotten all the 
+     valid fresh orange neighbours and puts them in the queue with an increase in time by 1 unit. Make sure to mark it as rotten in the visited array. 
+       
+---> Pop-out another rotten orange from the queue and repeat the same steps until the queue becomes empty.
+       
+---> Add a counter variable to store the maximum time and return it. If any of the fresh was not rotten in the visited array then return -1.
+
+ =====================================================================================================
+       
 Code Below:::::
 
 
 
-class Solution 
-{
-    public:
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
     //Function to find minimum time required to rot all oranges. 
-    int orangesRotting(vector<vector<int>>& grid) {
-        // Code here
-        queue<pair<pair<int, int>, int> > q;
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        int vis[n][m];
-        int totalCount = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(grid[i][j] == 2) 
-                {
-                    vis[i][j] = 2;
-                    q.push({{i, j}, 0});
-                }
-                else vis[i][j] = 0;
-                if(grid[i][j] == 1) totalCount++;
-            }
+    int orangesRotting(vector < vector < int >> & grid) {
+      // figure out the grid size
+      int n = grid.size();
+      int m = grid[0].size();
+
+      // store {{row, column}, time}
+      queue < pair < pair < int, int > , int >> q;
+      int vis[n][m];
+      int cntFresh = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          // if cell contains rotten orange
+          if (grid[i][j] == 2) {
+            q.push({{i, j}, 0}); 
+            // mark as visited (rotten) in visited array
+            vis[i][j] = 2;
+          }
+          // if not rotten
+          else {
+            vis[i][j] = 0;
+          }
+          // count fresh oranges
+          if (grid[i][j] == 1) cntFresh++;
         }
-        
-        int tm = 0;
-        int cnt = 0;
-        while(!q.empty())
-        {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            
-            tm = max(tm,t);
-            q.pop();
-            
-            int dr[] = {-1,0,1,0};
-            int dc[] = {0,1,0,-1};
-            
-            for(int i = 0; i < 4; i++)
-            {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                
-                if(nr >= 0 and nr < n and nc >= 0 and nc < m and grid[nr][nc] == 1 and vis[nr][nc] == 0)
-                {
-                    cnt++;
-                    q.push({{nr, nc}, t + 1});
-                    vis[nr][nc] = 2;
-                }
-            }
+      }
+
+      int tm = 0;
+      // delta row and delta column
+      int drow[] = {-1, 0, +1, 0};
+      int dcol[] = {0, 1, 0, -1}; 
+      int cnt = 0;
+
+      // bfs traversal (until the queue becomes empty)
+      while (!q.empty()) {
+        int r = q.front().first.first;
+        int c = q.front().first.second;
+        int t = q.front().second;
+        tm = max(tm, t);
+        q.pop();
+        // exactly 4 neighbours 
+        for (int i = 0; i < 4; i++) {
+          // neighbouring row and column
+          int nrow = r + drow[i];
+          int ncol = c + dcol[i];
+          // check for valid cell and 
+          // then for unvisited fresh orange
+          if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+            vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+            // push in queue with timer increased
+             q.push({{nrow, ncol}, t + 1}); 
+            // mark as rotten
+            vis[nrow][ncol] = 2;
+            cnt++;
+          }
         }
-        
-        if(totalCount != cnt) return -1;
-        return tm;
+      }
+
+      // if all oranges are not rotten
+      if (cnt != cntFresh) return -1;
+
+      return tm;
+
     }
 };
 
+int main() {
 
+  vector<vector<int>>grid{{0,1,2},{0,1,2},{2,1,1}};
+  Solution obj;
+  int ans = obj.orangesRotting(grid);
+  cout << ans << "\n";
 
-
-
-
-
+  return 0;
+}
 
 
 
