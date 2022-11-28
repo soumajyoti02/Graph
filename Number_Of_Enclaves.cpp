1ac3d74ffd53a0df1c8581b4cd799845839788c8
Soumajyoti Sarkar <103195837+soumajyoti02@users.noreply.github.com>
2022-11-28 19:43:16 +0530

    Create Number_Of_Enclaves.cpp

---
+++ Number_Of_Enclaves.cpp
@@ -0,0 +1,144 @@
+QUESTION : You are given an n x m binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+Find the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves;
+
+------------------------------------------------------------
+
+Input:
+grid[][] = {{0, 0, 0, 0},
+            {1, 0, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0}}
+Output:
+3
+Explanation:
+0 0 0 0
+1 0 1 0
+0 1 1 0
+0 0 0 0
+The highlighted cells represents the land cells;
+
+------------------------------------------------------------
+
+Input:
+grid[][] = {{0, 0, 0, 1},
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 1},
+            {0, 1, 1, 0}}
+Output:
+4
+Explanation:
+0 0 0 1
+0 1 1 0
+0 1 1 0
+0 0 0 1
+0 1 1 0
+The highlighted cells represents the land cells;
+
+
+==============================================================================================
+  
+  
+  APPROACH : 
+
+The algorithm steps are as follows:
+
+---> For BFS traversal, we need a queue data structure and a visited array. Create a corresponding visited array. 
+  
+---> Push the coordinates of boundary nodes in the queue and mark them as visited.
+  
+---> Start the BFS traversal, pop out an element from the queue every time and travel to all its unvisited 
+     neighboring land cells in the 4 directions. For every unvisited node, push it {row, col} into the Q 
+     and mark it as visited to avoid multiple traversals in the future. 
+  
+---> Repeat the steps until the queue becomes empty. When all the boundaries are traversed and corresponding 
+     sets of 1s are marked as visited, use a counter variable to count the number of remaining unvisited land cells. 
+  
+---> Return the value of the counter as it indicates the number of land cells that cannot cross the boundary.
+
+=-==========================================================================================================================
+
+CODE BELOW :::::
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        queue<pair<int,int>> q; 
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        int vis[n][m] = {0}; 
+        // traverse boundary elements
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // first row, first col, last row, last col 
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    // if it is a land then store it in queue
+                    if(grid[i][j] == 1) {
+                        q.push({i, j}); 
+                        vis[i][j] = 1; 
+                    }
+                }
+            }
+        }
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.empty()) {
+            int row = q.front().first; 
+            int col = q.front().second; 
+            q.pop(); 
+            
+            // traverses all 4 directions
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+                // check for valid coordinates and for land cell
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1; 
+                }
+            }
+            
+        }
+        
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // check for unvisited land cell
+                if(grid[i][j] == 1 & vis[i][j] == 0) 
+                    cnt++; 
+            }
+        }
+        return cnt; 
+    }
+};
+
+int main() {
+    
+    vector<vector<int>> grid{
+        {0, 0, 0, 0},
+        {1, 0, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0}};
+        
+    Solution obj;
+    cout << obj.numberOfEnclaves(grid) << endl;
+
+}
+
+
+
+
+
+
+
+
+
